846d157f956db0ec2b0b097780241720f5c91833
Alice Smith <alice@webpage.com>
2025-06-30 15:54:59 -0400

    Fixed server.js file, browser can see json data

--- server/server.js
+++ server/server.js
@@ -1,14 +1,27 @@
 import express from 'express';
 import cors from 'cors';
+import { MongoClient, ObjectId } from 'mongodb';
+import dotenv from 'dotenv';
 
+dotenv.config();
+const url = process.env.MONGO_DB_URL;
+const dbName = process.env.MONGO_DB;
+const collectionPlanet = process.env.MONGO_DB_PLANETS;
+const collectionCharacter = process.env.MONGO_DB_CHARACTERS;
+const collectionFilms = process.env.MONGO_DB_FILMS
 const app = express();
 app.use(cors()); // Enable CORS for all routes
 app.use(express.json()); // Middleware to parse JSON bodies
 const PORT = 3000;
 
-app.get('/', async (req, res) => {
+//Path and input data for planets
+app.get('/planets', async (req, res) => {
     try {
-        res.json("Running server!");
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection('planets')
+        const planets = await collection.find({}).toArray();
+        res.json(planets);
     } catch (err) {
         console.error("Error:", err);
         res.status(500).send("Server failed");
@@ -17,7 +30,117 @@ app.get('/', async (req, res) => {
 
 app.get('/api/planets', async (req, res) => {
     try {
-        res.json("Planet data");
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection('planets')
+        const planets = await collection.find({}).toArray();
+        res.json(planets);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Could not load data for planets page");
+    }
+});
+//Path and input data for Characters
+app.get('/character', async (req, res) => {
+    try {
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection('characters')
+        const planets = await collection.find({}).toArray();
+        res.json(characters);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Server failed");
+    }
+});
+
+app.get('/api/characters', async (req, res) => {
+    try {
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection('characters')
+        const characters = await collection.find({}).toArray();
+        res.json(characters);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Could not load data for planets page");
+    }
+});
+//Path and input data for films
+app.get('/films', async (req, res) => {
+    try {
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection('films')
+        const films = await collection.find({}).toArray();
+        res.json(films);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Server failed");
+    }
+});
+
+app.get('/api/films', async (req, res) => {
+    try {
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection('films')
+        const films = await collection.find({}).toArray();
+        res.json(films);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Could not load data for planets page");
+    }
+});
+//Path and input data for films_characters
+app.get('/films_characters', async (req, res) => {
+    try {
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection('films_characters')
+        const films_characters = await collection.find({}).toArray();
+        res.json(films_characters);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Server failed");
+    }
+});
+
+app.get('/api/filmsCharacters', async (req, res) => {
+    try {
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection('films_characters')
+        const films_characters = await collection.find({}).toArray();
+        res.json(films_characters);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Could not load data for planets page");
+    }
+});
+
+
+//Path and input data for films_planets
+app.get('/films_planets', async (req, res) => {
+    try {
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection('films_planets')
+        const films_planets = await collection.find({}).toArray();
+        res.json(films_planets);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Server failed");
+    }
+});
+
+app.get('/api/filmsPlanets', async (req, res) => {
+    try {
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection('films_planets')
+        const films_planets = await collection.find({}).toArray();
+        res.json(films_planets);
     } catch (err) {
         console.error("Error:", err);
         res.status(500).send("Could not load data for planets page");

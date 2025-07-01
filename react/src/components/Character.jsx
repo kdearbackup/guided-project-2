2d6f1ecb499d37dd86b16eba0ed0069b9296d936
Alice Smith <alice@webpage.com>
2025-07-01 12:04:42 -0400

    changing character.jsx ...

--- react/src/components/Character.jsx
+++ react/src/components/Character.jsx
@@ -6,10 +6,13 @@ import "../App.css"
 
 function Character() {
   const [data, setData] = useState(0)
+  const [planet, setPlanetData] = useState(0)
+  const [film, setFilmData] = useState(0)
   let { id } = useParams();
   id = id.replace(/:/g, "")
   id = parseInt(id)
   const URL  = `http://localhost:3000/api/characters`
+  
 
   useEffect(() => {
     const fetchData = async () => {
@@ -33,7 +36,58 @@ function Character() {
     };
 
     fetchData(); 
-  }, []); 
+  }, [id]); 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        console.log(data)
+        const URL2 = `http://localhost:3000/api/planets/${data.homeworld}`
+        const response = await fetch(URL2);
+         
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const json = await response.json();
+
+        console.log(json)
+
+        console.log(id)
+
+        
+        setPlanetData(json); 
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData(); 
+  }, [data]); 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        console.log(data)
+        const URL3 = `http://localhost:3000/api/characters/${id}/films`
+        const response = await fetch(URL3);
+         
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const json = await response.json();
+
+        console.log(json)
+
+        console.log(id)
+
+        
+        setFilmData(json); 
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData(); 
+  }, [data]);
+  
 
   //if (!data) {
     //return <div>Loading...</div>; // Display loading state
@@ -49,6 +103,17 @@ function Character() {
       </>
     )
   }
+  if (!data.homeworld){
+    return (
+      <>
+      <div>
+        <h1>No planet found</h1>
+        <div className="backCard"><Link to= "/"> Back </Link></div>
+      </div>
+      </>
+    )
+  }
+  
 
   return (
 
@@ -61,7 +126,7 @@ function Character() {
         <div className="card">Born: {data.birth_year}</div>
 
         <h2>Homeworld</h2>
-        <div className="card">Homeworld Id: {data.homeworld} (Need to implement)</div>
+        <div className="card">: {planet?.name} (Need to implement)</div>
         <h2>Films Appeared In</h2>
         <div className="card">(Need to implement)</div>
 
